17f12c799063c97cea0cfe2f7b20cbb235ea9a4f
Kaitian Xie <xkaitian@gmail.com>
2018-11-10 05:24:21 -0800

    Update server

--- src/server.ts
+++ src/server.ts
@@ -7,27 +7,51 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 const port: string = process.env.PORT || "3000";
-const dbLocation: string = "../db.json";
+const dbLocation: string = "./db.json";
+
+let userCount: number = 0;
 
 server.listen(port, () => {
   console.log(`Listening at port: ${port}`);
 });
 
+server.on("error", (err: any) => {
+  if (err.code === "EADDRINUSE") {
+    console.log("Address in use, retrying...");
+    setTimeout(() => {
+      server.close();
+      server.listen(port);
+    }, 1000);
+  }
+});
+
 io.on("connection", socket => {
+  userCount++;
+  console.log(`${userCount} users are connected`);
+
   socket.on("add data", data => {
-    const d: string = JSON.stringify(data["message"]);
+    console.log(`data: ${data}`);
 
-    fs.readFile(dbLocation, (err, currData) => {
-      const json: any[] = JSON.parse(currData.toString());
-      json.push(data);
-
-      fs.writeFile(dbLocation, JSON.stringify(json), err => {
+    if (data) {
+      fs.readFile(dbLocation, (err, currData) => {
         if (err) {
           console.log(err);
         }
 
-        console.log("New data added to DB");
+        const json: any[] = JSON.parse(currData.toString());
+        json.push({ message: data });
+
+        fs.writeFile(dbLocation, JSON.stringify(json), err => {
+          if (err) {
+            console.log(err);
+          }
+        });
       });
-    });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    userCount--;
+    console.log(`${userCount} users are connected`);
   });
 });
