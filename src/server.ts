71f603ab83376a9a54a27c12687005ea5ade7a15
Kaitian Xie <xkaitian@gmail.com>
2018-11-10 03:52:22 -0800

    Now user can add data to DB

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,33 @@
-console.log("hello");
+import * as express from "express";
+import * as http from "http";
+import * as socketio from "socket.io";
+import * as fs from "fs";
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const port: string = process.env.PORT || "3000";
+const dbLocation: string = "../db.json";
+
+server.listen(port, () => {
+  console.log(`Listening at port: ${port}`);
+});
+
+io.on("connection", socket => {
+  socket.on("add data", data => {
+    const d: string = JSON.stringify(data["message"]);
+
+    fs.readFile(dbLocation, (err, currData) => {
+      const json: any[] = JSON.parse(currData.toString());
+      json.push(data);
+
+      fs.writeFile(dbLocation, JSON.stringify(json), err => {
+        if (err) {
+          console.log(err);
+        }
+
+        console.log("New data added to DB");
+      });
+    });
+  });
+});
