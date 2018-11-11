10aa61b25ee151ae182f1f43e31f87963fc83eab
Kaitian Xie <xkaitian@gmail.com>
2018-11-11 12:16:50 -0800

    Add a test case

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,4 +1,5 @@
 import Server from "./server";
+import * as io from "socket.io-client";
 
 const filePath: string = "./db.json";
 const port: string = "3000";
@@ -8,4 +9,21 @@ let server: Server;
 beforeAll(done => {
   server = new Server(filePath, port);
   done();
+});
+
+afterAll(done => {
+  server.close();
+  done();
+});
+
+test("Client can add data to the server", () => {
+  const data: string = "hello";
+  const client: SocketIOClient.Socket = io("http://localhost:3000/");
+  client.connect();
+  const observer: SocketIOClient.Socket = io("http://localhost:3000/");
+  observer.connect();
+  observer.on("update", (res: string) => {
+    expect(res).toBe(data);
+  });
+  client.send("add data", data);
 });
