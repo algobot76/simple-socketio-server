5c9472faa3af9bc34beb2b91adb11dad7cd27b83
Kaitian Xie <xkaitian@gmail.com>
2018-11-11 12:16:38 -0800

    Remove comments

--- src/server.ts
+++ src/server.ts
@@ -1,71 +1,3 @@
-// import * as express from "express";
-// import * as http from "http";
-// import * as socketio from "socket.io";
-// import * as fs from "fs";
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketio(server);
-// const port: string = process.env.PORT || "3000";
-// const dbLocation: string = "./db.json";
-
-// let userCount: number = 0;
-
-// server.listen(port, () => {
-//   console.log(`Listening at port: ${port}`);
-// });
-
-// server.on("error", (err: any) => {
-//   if (err.code === "EADDRINUSE") {
-//     console.log("Address in use, retrying...");
-//     setTimeout(() => {
-//       server.close();
-//       server.listen(port);
-//     }, 1000);
-//   }
-// });
-
-// io.on("connection", socket => {
-//   userCount++;
-//   console.log(`${userCount} users are connected`);
-
-//   socket.on("add data", data => {
-//     console.log(`data: ${data}`);
-
-//     if (data) {
-//       const usrIpAddr: string = socket.handshake.address;
-
-//       fs.readFile(dbLocation, (err, currData) => {
-//         if (err) {
-//           console.log(err);
-//         }
-
-//         const json: any[] = JSON.parse(currData.toString());
-//         const entry: any = {
-//           usrIpAddr,
-//           message: data,
-//         };
-//         json.push(entry);
-
-//         fs.writeFile(dbLocation, JSON.stringify(json), err => {
-//           if (err) {
-//             console.log(err);
-//           }
-//         });
-
-//         socket.broadcast.emit("update", entry);
-//       });
-//     }
-//   });
-
-//   socket.on("disconnect", () => {
-//     userCount--;
-//     console.log(`${userCount} users are connected`);
-//   });
-// });
-
-// export default io;
-
 import * as express from "express";
 import * as http from "http";
 import * as socketio from "socket.io";
@@ -149,6 +81,7 @@ export default class Server {
 
   public close(): void {
     console.log("Closing the server");
+    this.io.close();
     this.server.close();
   }
 }
