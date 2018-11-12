91847e032127fd14e623a27ae79721bd85d8a0ed
Kaitian Xie <xkaitian@gmail.com>
2018-11-12 11:51:06 -0800

    Create a successful test

--- src/server.test.ts
+++ src/server.test.ts
@@ -5,9 +5,11 @@ const filePath: string = "./db.json";
 const port: string = "3000";
 
 let server: Server;
+let client: SocketIOClient.Socket;
 
 beforeAll(done => {
   server = new Server(filePath, port);
+  server.open();
   done();
 });
 
@@ -16,14 +18,29 @@ afterAll(done => {
   done();
 });
 
-test("Client can add data to the server", () => {
-  const data: string = "hello";
-  const client: SocketIOClient.Socket = io("http://localhost:3000/");
-  client.connect();
-  const observer: SocketIOClient.Socket = io("http://localhost:3000/");
-  observer.connect();
-  observer.on("update", (res: string) => {
-    expect(res).toBe(data);
+beforeEach(done => {
+  client = io.connect(
+    "http://localhost:3000/",
+    { forceNew: true, transports: ["websocket"] },
+  );
+  client.on("connect", () => {
+    done();
   });
-  client.send("add data", data);
+});
+
+afterEach(done => {
+  if (client.connected) {
+    client.disconnect();
+  }
+  done();
+});
+
+describe("server test", () => {
+  test("Client can add data to the server", () => {
+    const data: string = "hello";
+    client.once("update", (message: string) => {
+      expect(message).toBe(data);
+    });
+    client.send("add data", data);
+  });
 });
